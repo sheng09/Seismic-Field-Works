82de77af89323b7dff92479b05378e1aef5362a9
sheng09 <wangsheng.cas@gmail.com>
2015-09-25 17:12:55 +0800

    move linearStack() and nrootStack() to libnumrec.c

--- src/NRootStack.c
+++ src/NRootStack.c
@@ -3,6 +3,9 @@
 #include <math.h>
 #include <string.h>
 #include "sac.h"
+#include "libnumrec.h"
+
+#define MAXSTACKFILE 1000
 
 char HMSG[] = "Usage: %s -O<output sac file> -N<N-th root>  <Input file list>\n";
 
@@ -14,10 +17,11 @@ int main(int argc, char *argv[])
         float NRoot = 0.0;
         char *strOut = NULL;
         int  fgout  = 0;
-        int  i,j,num = 0;
+        int  i,num = 0;
         SACHEAD sachdr;
         SACHEAD hdrout = sac_null;
         float   *data, *ak = NULL;
+        float   *lst[MAXSTACKFILE];
         for(i = 1; i < argc; ++i)
         {
                 if(argv[i][0] == '-')
@@ -66,37 +70,32 @@ int main(int argc, char *argv[])
 
                 read_sachead(argv[i], &sachdr);
                 data = read_sac( argv[i], &sachdr );
+                lst[num] = data;
                 ++num;
-                for(j = 0; j < sachdr.npts; ++j)
-                {
-                    //Add by Wangsheng 2015/09/04
-                    //printf("%f\n", data[i]);
-                    if( fabs(data[j]) > 1.0e-6 )
-                    {
-                            ak[j] +=  (expf(logf( fabsf(data[j]) ) / NRoot ) ) * ( data[j] / fabsf(data[j]) );
-                      //      printf("hello\n");
-                    }
-
-                    //if( data[j] - 0.0 > 1.0e-7)
-                    //    ak[j] +=  (expf(logf( fabsf(data[j]) ) / NRoot ) );
-                    //else if( data[j] - 0.0 < 1.0e-7)
-                    //    ak[j] +=  (expf(logf( fabsf(data[j]) ) / NRoot ) * -1.0);
-                }
+                // Commented by wangsheng 2015/09/18. Now use a 'stack' function in numrec library
+                //for(j = 0; j < sachdr.npts; ++j)
+                //{
+                //Add by Wangsheng 2015/09/04
+                //    //printf("%f\n", data[i]);
+                //    if( fabs(data[j]) > 1.0e-6 )
+                //    {
+                //            ak[j] +=  (expf(logf( fabsf(data[j]) ) / NRoot ) ) * ( data[j] / fabsf(data[j]) );
+                //    }
+                //}
                 hdrout.user0 += sachdr.user0;
-                //printf("%f ",sachdr.user0 );
-                //printf("%f %f\n", data[0], ak[0] );
-                free(data);
+                //free(data);
             }
         }
 
+        //Add by wangsheng 2015/09/18
+        nrootStack(lst, num, hdrout.npts, &ak, NRoot);
+        //
         hdrout.user0 /= (float)(num);
-        //printf("\n%f\n", hdrout.user0);
-        for(j = 0; j < sachdr.npts; ++j)
-        {
-            ak[j] = ( ak[j] /  (float)(num) );
-            ak[j] = powf(fabsf(ak[j]), NRoot - 1.0) * ak[j];
-        }
-        //ak[j] =  powf( (ak[j]) / (float)(num), NRoot - 1 ) * ak[j];
+        //for(j = 0; j < sachdr.npts; ++j)
+        //{
+        //    ak[j] = ( ak[j] /  (float)(num) );
+        //    ak[j] = powf(fabsf(ak[j]), NRoot - 1.0) * ak[j];
+        //}
         write_sac(strOut, hdrout, ak);
         free(ak);
         return 0;
